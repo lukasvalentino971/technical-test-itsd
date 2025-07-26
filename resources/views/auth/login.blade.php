639ef146fbf0d4a51fd53604cc9e79a2523e47d7
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2025-07-26 08:29:13 +0700

    fix : field email at login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,8 @@
                                         <form action="{{ route('login') }}" method="POST">
                                             @csrf
                                             <div class="mb-3">
-                                                <label for="username" class="form-label">Username <span class="text-danger">*</span></label>
-                                                <input type="text" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', 'admin@themesbrand.com') }}" id="username" name="email" placeholder="Enter username">
+                                                <label for="username" class="form-label">Email <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="username" name="email" placeholder="Enter email">
                                                 @error('email')
                                                     <span class="invalid-feedback" role="alert">
                                                         <strong>{{ $message }}</strong>
@@ -65,7 +65,7 @@
                                             <div class="mb-3">
                                                 <label class="form-label" for="password-input">Password <span class="text-danger">*</span></label>
                                                 <div class="position-relative auth-pass-inputgroup mb-3">
-                                                    <input type="password" class="form-control password-input pe-5 @error('password') is-invalid @enderror" name="password" placeholder="Enter password" id="password-input" value="12345678">
+                                                    <input type="password" class="form-control password-input pe-5 @error('password') is-invalid @enderror" name="password" placeholder="Enter password" id="password-input">
                                                     <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                                     @error('password')
                                                         <span class="invalid-feedback" role="alert">
